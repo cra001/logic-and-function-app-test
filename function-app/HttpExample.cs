eb6ad2f03c312d271a7bbb39afe9a0547e726f38
Casper Rødgaard <cra001@narratech.net>
2023-03-07 13:06:42 +0100

    Change to HttExample function

--- function-app/HttpExample.cs
+++ function-app/HttpExample.cs
@@ -22,7 +22,7 @@ namespace My.Functions
             name = name ?? data?.name;
 
             string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                ? "What in tarnation?!"
                 : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
             return new OkObjectResult(responseMessage);
